8003a808d48ff503371536548b7ab069590d4cc0
ayeseeem <ayeseeem@gmail.com>
2021-01-02 16:23:08 +0000

    doc: fix typo in comment

--- src/main/java/org/ayeseeem/matchers/ConvertibleStringMatchers.java
+++ src/main/java/org/ayeseeem/matchers/ConvertibleStringMatchers.java
@@ -115,7 +115,7 @@ public class ConvertibleStringMatchers {
             @Override
             public boolean matchesSafely(String possible) {
                 try {
-                    // TODO: ICM 2019-03-22: Split booleanOfValue into booleanOf() and booleanOfValue()? - check it's a number
+                    // TODO: ICM 2019-03-22: Split booleanOfValue into booleanOf() and booleanOfValue()? - check it's a boolean
                     final boolean actual = Boolean.parseBoolean(possible);
                     return actual == expected;
                 } catch (NumberFormatException e) {
