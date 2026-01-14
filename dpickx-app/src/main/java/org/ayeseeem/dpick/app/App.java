343564a9eaf0bf70aa1bcaaccd95ff4910987cdf
ayeseeem <ayeseeem@gmail.com>
2026-01-14 17:00:25 +0000

    feat(CLI): add -i --integer: capture as integer
    Adding -i (or long-form, --integer) attempts to capture the content of
    the XPath as an integer, showing an error if the content is not found,
    or is found but cannot be parsed as an integer.
    
    Still uses a hard-coded findSoleRequired command. Not yet supporting
    optional, or find first, or find list and so on.

--- dpickx-app/src/main/java/org/ayeseeem/dpick/app/App.java
+++ dpickx-app/src/main/java/org/ayeseeem/dpick/app/App.java
@@ -5,6 +5,8 @@ import static org.ayeseeem.dpick.xml.NodeMatchers.xpath;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.xml.xpath.XPathExpressionException;
 
@@ -22,25 +24,30 @@ public class App {
     private final String file;
     private final String xpath;
 
-    public App(String file, String xpath) throws XPathExpressionException, FileNotFoundException, IOException, SAXException {
+    public App(String file, String xpath, boolean asInteger) throws XPathExpressionException, FileNotFoundException, IOException, SAXException {
         this.file = file;
         this.xpath = xpath;
 
-        captureAndPrint();
+        captureAndPrint(asInteger);
     }
 
-    private void captureAndPrint() throws XPathExpressionException, FileNotFoundException, IOException, SAXException {
+    private void captureAndPrint(boolean asInteger) throws XPathExpressionException, FileNotFoundException, IOException, SAXException {
         Node docRoot = makeDoc(file);
 
         XmlDocumentChecker checker = new XmlDocumentChecker(docRoot);
 
-        String content = checker.captureSoleRequired(xpath(xpath));
-        System.out.println(content);
+        if (asInteger) {
+            int content = checker.captureSoleRequired(xpath(xpath), Integer::parseInt);
+            System.out.println(content);
+        } else {
+            String content = checker.captureSoleRequired(xpath(xpath));
+            System.out.println(content);
+        }
     }
 
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println("Need 2 args: <filename> <XPath>");
+            System.out.println("Need at least 2 args: <filename> <XPath> [-i] [--integer]");
             System.out.println(" - Xpath might need (Java) escaping of characters such as \\");
             return;
         }
@@ -48,8 +55,11 @@ public class App {
         String file = args[0];
         String xpath = args[1];
 
+        List<String> argList = Arrays.asList(args);
+        boolean asInteger = argList.contains("-i") || argList.contains("--integer");
+
         try {
-            new App(file, xpath);
+            new App(file, xpath, asInteger);
         } catch (XPathExpressionException | IOException | SAXException e) {
             e.printStackTrace();
         }
